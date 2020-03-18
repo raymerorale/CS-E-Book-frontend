181e12f35f9e233bfa96785c3f60bf863d9b2b7b
Nica <pereav110@gmail.com>
2020-03-18 17:49:46 +0800

    Remove lorem ipsum

--- v2/app/src/constants/chapters/ch4/Chapter4.js
+++ v2/app/src/constants/chapters/ch4/Chapter4.js
@@ -4,8 +4,8 @@ export const CHAPTER4 = {
 				<h2 align = center>BASIC FEATURE OF MICROPROCESSOR</h2>`,
 	sub: [
 		{
-			title: 'Sample Title',
-			content: `Number System sample content Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.`,
+			title: '',
+			content: ``,
 		},
 	]
 };
