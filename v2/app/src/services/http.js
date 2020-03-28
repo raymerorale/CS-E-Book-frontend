e8b19d31152e621f77cc1f70afa6a90bc3444ce8
Nica <pereav110@gmail.com>
2020-03-28 22:22:19 +0800

    Correct typos

--- v2/app/src/services/http.js
+++ v2/app/src/services/http.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const http = axios.create({
-  baseURL: 'http://locahost:4000/',
+  baseURL: 'http://localhost:4000/',
   headers: {
     'Content-Type': 'application/json'
   }
--- v2/app/src/store/actions.js
+++ v2/app/src/store/actions.js
@@ -2,7 +2,7 @@ import http from '@/services/http'
 
 export default {
 	login: ({ commit }, payload) => {
-		http.post('user/authentication', payload).then(user => {
+		http.post('user/authenticate', payload).then(user => {
 			if (user.token) {
 				commit('USER', user)
 			}
