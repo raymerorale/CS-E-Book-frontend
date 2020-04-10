a6bf524851bf11405b2f1a038c8df18e2dd16096
Veronica Perea <pereav110@gmail.com>
2020-04-10 20:03:22 +0800

    fix

--- v2/app/src/helpers/chapters.js
+++
@@ -1,9 +0,0 @@
-export function findChapterById(data, id) {
-	for(var i = 0; i < data.length; i++) {
-			if (data[i].chapterId === id) {
-					return data[i];
-			} else if (data[i].sub && data[i].sub.length && typeof data[i].sub === "object") {
-					findChapterById(data[i].sub, id);
-			}
-	}
-}
--- v2/app/src/store/mutations.js
+++ v2/app/src/store/mutations.js
@@ -1,4 +1,12 @@
-import { findChapterById } from '@/helpers/chapters'
+const findChapterById = (state, id) => {
+	for(var i = 0; i < state.chapters.length; i++) {
+			if (state.chapters[i].chapterId == id) {
+					return state.chapters[i];
+			} else if (state.chapters[i].sub && state.chapters[i].sub.length) {
+					findChapterById(state.chapters[i].sub, id);
+			}
+	}
+}
 
 export default {
 	USER: (state, user) => {
@@ -7,19 +15,12 @@ export default {
 	},
 	SET_USER_CHAPTERS_ENABLED: (state, chapter_ids) => {
 		chapter_ids.map(id =>{
-			let chapter = findChapterById(state.chapters, id)
+			let chapter = findChapterById(state, id)
 			if (chapter) {
 				chapter.read_status = 'Done'
 			}
 		})
 	},
 	SET_READ_STATUS: (state, read_status) => state.read_status = read_status,
-	CREATE_READ_STATUS: (state, read_status) => state.read_status.push(read_status),
-	UPDATE_READ_STATUS: (state, read_status) => {
-		let item = state.read_status.find(item => item.id == read_status.id)
-		if (item) {
-			item.status = read_status.status
-		}
-	},
 	updateChapters: (state, value) => state.chapters = value,
 }	
